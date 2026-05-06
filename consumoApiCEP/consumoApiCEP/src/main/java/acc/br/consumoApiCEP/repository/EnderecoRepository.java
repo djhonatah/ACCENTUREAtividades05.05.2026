067aaead04fa28f9cb50d1b45d7fea68534a9278
Djhonatah Wesley <djhonatahwesley2402@gmail.com>
2026-05-06 12:08:26 -0300

    refactor: remove deprecated and unused utility files

--- consumoApiCEP/consumoApiCEP/src/main/java/acc/br/consumoApiCEP/repository/EnderecoRepository.java
+++
@@ -1,7 +0,0 @@
-package acc.br.consumoApiCEP.repository;
-
-import acc.br.consumoApiCEP.model.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-}
--- consumoApiCEP/consumoApiCEP/src/main/java/acc/br/consumoApiCEP/service/CepService.java
+++
@@ -1,7 +0,0 @@
-package acc.br.consumoApiCEP.service;
-
-import acc.br.consumoApiCEP.model.Endereco;
-
-public interface CepService {
-    Endereco buscarESalvar(String cep);
-}
